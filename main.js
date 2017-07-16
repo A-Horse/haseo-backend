16080fbe8c59297a084d8fa29df0978e427cd317
fwchen <crystal.tyan@gmail.com>
2017-07-16 17:13:22 +0800

    initial commit

--- main.js
+++ main.js
@@ -2,30 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import bluebird from 'bluebird';
 import YAML from 'yamljs';
+import systemConfig from './systemConfig';
+import R from 'ramda';
 
 import Observer from './lib/repo-observer';
 import DispatcherInstance from './lib/dispatcher';
+import ProjectManager from './lib/project-manager';
 
 class Controller {
   constructor() {
-    this.dispatcher = DispatcherInstance;
-    this.projectConfigs = this.readProjectConfigs(
-      systemConfig.projectConfigsPath
-    );
-    this.observer = new Observer(this.projectConfigs);
-  }
-
-  readProjectConfigs(storePath) {
-    return fs
-      .readdirSync(storePath, 'utf-8')
-      .filter(p => /.yaml$/.test(p))
-      .map(configFilePath => {
-        return YAML.load(path.join(storePath, configFilePath));
-      });
+    this.projectManager = new ProjectManager();
   }
 
   startup() {}
+
+  restart() {}
 }
 
 const ctrl = new Controller();
-ctrl.startup();
