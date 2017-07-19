d4438e2efd11a0e40c3bc313fbe670b49c155ce2
fwchen <crystal.tyan@gmail.com>
2017-07-19 11:40:50 +0800

    initial commit

--- systemConfig.js
+++ systemConfig.js
@@ -2,7 +2,7 @@
 const systemConfig = {
   repoStoragePath: '/Users/chchen/CI-Storage',
   projectConfigsPath: '/Users/chchen/CI-Haseo/project-yaml',
-  repoObserverInterval: 3 * 1000
+  repoObserverInterval: 20 * 1000
 };
 
 export default systemConfig;
