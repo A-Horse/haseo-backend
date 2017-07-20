78603c9c3e0662ce385ff9f21d7faddcaaf4a30a
fwchen <crystal.tyan@gmail.com>
2017-07-20 21:11:42 +0800

    initial commit

--- systemConfig.js
+++ systemConfig.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 const systemConfig = {
   repoStoragePath: path.join(__dirname, 'REPOS'),
-  repoObserverInterval: 20 * 1000
+  repoObserverInterval: 60 * 1000
 };
 
 export default systemConfig;
