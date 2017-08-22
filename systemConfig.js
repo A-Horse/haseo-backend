f682f12a9a25729fa22f9b12137c735b1899798c
fwchen <crystal.tyan@gmail.com>
2017-08-22 21:04:31 +0800

    update watch interval time

--- systemConfig.js
+++ systemConfig.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 const systemConfig = {
   repoStoragePath: path.join(__dirname, 'REPOS'),
-  repoObserverInterval: 60 * 1000
+  repoObserverInterval: 6 * 1000
 };
 
 export default systemConfig;
