c18b6e86a2d8f9946fd3564b31d8e95f551ec00a
fwchen <crystal.tyan@gmail.com>
2017-09-12 14:34:44 +0800

    add logger

--- test/project.test.js
+++ test/project.test.js
@@ -1,10 +1,11 @@
 import Project from '../lib/project';
+import path from 'path';
 
 describe('Project', () => {
 
   test('project', () => {
-    const project =  new Project('./__mock_project__/', 'TEST-ONE');
-
+    console.log(path.join(__dirname, '/__mock_project__/'));
+    const project =  new Project(path.join(__dirname, '/__mock_project__/'), 'TEST-ONE');
   });
 
 
--- util/logger.js
+++ util/logger.js
@@ -1,11 +1,15 @@
 import winston from 'winston';
+import moment from 'moment';
 
 const logger = new winston.Logger({
-  transports: [new winston.transports.Console({ level: 'info' })({
-    timestamp: function() {
-      return Date.now();
-    }
-  })]
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+      timestamp: function() {
+        return moment().format('YYYY-MM-DD HH:mm:ss');
+      }
+    })
+  ]
 });
 
 export default logger;
