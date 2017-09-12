78cc883cfd42ef5248175ecb0facc3be8e13a041
fwchen <crystal.tyan@gmail.com>
2017-09-12 11:53:16 +0800

    test mock source initial

---
+++ test/project.test.js
@@ -0,0 +1,11 @@
+import Project from '../lib/project';
+
+describe('Project', () => {
+
+  test('project', () => {
+    const project =  new Project('./__mock_project__/', 'TEST-ONE');
+
+  });
+
+
+});
