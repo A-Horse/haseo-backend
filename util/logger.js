2cf0492219a5d301d107eb796b3a3f0762742fa4
fwchen <crystal.tyan@gmail.com>
2017-09-12 11:53:32 +0800

    haseo yaml

--- util/logger.js
+++ util/logger.js
@@ -1,7 +1,11 @@
 import winston from 'winston';
 
 const logger = new winston.Logger({
-  transports: [new winston.transports.Console({ level: 'info' })]
+  transports: [new winston.transports.Console({ level: 'info' })({
+    timestamp: function() {
+      return Date.now();
+    }
+  })]
 });
 
 export default logger;
