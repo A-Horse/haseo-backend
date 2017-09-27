f63c4df79c5263ba07def433aa1ab9aac1ba9fff
fwchen <crystal.tyan@gmail.com>
2017-09-27 23:13:28 +0800

    change interval

--- systemConfig.js
+++ systemConfig.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 const systemConfig = {
   repoStoragePath: path.join(__dirname, 'REPOS'),
-  repoObserverInterval: 10 * 1000
+  repoObserverInterval: 20 * 1000
 };
 
 export default systemConfig;
