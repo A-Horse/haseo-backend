3f1ba3d1b6a31c922cbe50fc9160560d29d6fb68
fwchen <crystal.tyan@gmail.com>
2017-10-24 12:09:23 +0800

    rename wrong spell

---
+++ src/service/knex.js
@@ -0,0 +1,9 @@
+import path from 'path';
+
+export const knex = require('knex')({
+  client: 'sqlite3',
+  connection: { filename: path.join(__dirname, '../../db.sqlite3') },
+  useNullAsDefault: true
+});
+
+export default knex;
