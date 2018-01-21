50f3fd4405c3af9d2988585769950c3c0d9f28a9
fwchen <crystal.tyan@gmail.com>
2018-01-21 22:18:13 +0800

    update

--- src/ws-subscription/auth.subscription.ts
+++ src/ws-subscription/auth.subscription.ts
@@ -2,11 +2,13 @@ import './of-type.operator';
 import { Subject } from 'rxjs/Subject';
 import { CIDaemon } from 'src/ci';
 import { WebSocketHelper } from 'src/socket/websocket-helper';
+import { verityJwt } from '../service/auth';
+
 
 export const WS_AUTH_REQUEST = (message$: Subject<SocketMessage>, ws: WebSocketHelper, ciCtrlDaemon: CIDaemon) =>
   message$.ofType('WS_AUTH_REQUEST').subscribe((message: SocketMessage) => {
 
-    const user = verityJwt(event.payload).data;
+    const user = verityJwt(message.payload).data;
     isAuth = true;
     
     ws.sendJSON({
