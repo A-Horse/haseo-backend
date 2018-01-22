b2268ecee2f4721e09cd4529eacf145b27175ff4
fwchen <crystal.tyan@gmail.com>
2018-01-22 23:33:53 +0800

    update

--- src/socket/ws-subscriber/index.ts
+++ src/socket/ws-subscriber/index.ts
@@ -5,14 +5,21 @@ import { WebSocketHelper } from 'src/socket/websocket-helper';
 import { CIDaemon } from 'src/ci';
 import { setupMessageInterceptor } from './message-interceptor';
 
-export function setupWebsocketSubscriber(message$, wsh: WebSocketHelper, ciCtrlDaemon: CIDaemon): void {
+export function setupWebsocketSubscriber(
+  message$,
+  wsh: WebSocketHelper,
+  ciCtrlDaemon: CIDaemon
+): void {
   const dirpath = path.join(__dirname);
-  const wsSubscriptionModules = fs.readdirSync(dirpath).filter(filename => /subscriber.js$/.test(filename)).map(filename => {
-    return require(path.join(dirpath, filename));
-  });
+  const wsSubscriptionModules = fs
+    .readdirSync(dirpath)
+    .filter(filename => /subscriber.js$/.test(filename))
+    .map(filename => {
+      return require(path.join(dirpath, filename));
+    });
   const subscriptionFns = R.compose(R.flatten, R.map(R.values))(wsSubscriptionModules);
 
-  setupMessageInterceptor(message$, wsh);
-  subscriptionFns.forEach(subscriptionFn => subscriptionFn(message$, wsh, ciCtrlDaemon))
-}
+  const filteredMessage$ = setupMessageInterceptor(message$, wsh);
 
+  subscriptionFns.forEach(subscriptionFn => subscriptionFn(filteredMessage$, wsh, ciCtrlDaemon));
+}
