b5ef2228ed066e6c2a225b1d19cda5e4baa4c40c
fwchen <crystal.tyan@gmail.com>
2018-03-02 15:38:27 +0800

    update

--- src/router/user.ts
+++ src/router/user.ts
@@ -12,7 +12,7 @@ UserRouter.post('/logout', (req, res) => {
 });
 
 UserRouter.get('/self-info', async (req, res, next) => {
-  const jwt = req.headers['jwt'];
+  const jwt = req.header['jwt'];
   try {
     const user = verityJwt(jwt).data;
     res.send(user);
