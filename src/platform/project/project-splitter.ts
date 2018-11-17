a2ed2881bcec1714ba6e512b074ae7b98078512f
fwchen <crystal.tyan@gmail.com>
2018-11-17 15:33:14 +0800

    fix bug for select project

--- src/platform/project/project-splitter.ts
+++ src/platform/project/project-splitter.ts
@@ -3,6 +3,6 @@ import * as fs from 'fs';
 
 export function projectSplit(repoPath: string): Project[] {
   return fs.readdirSync(repoPath)
-    .filter(p => /^haseo(\.[a-zA-Z0-9]+)?\.yaml$/.test(p))
+    .filter(p => /^\.?haseo(\.[a-zA-Z0-9]+)?\.yaml$/.test(p))
     .map(configFile => new Project(repoPath, configFile));
 }
